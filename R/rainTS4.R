d29f4cedcf4c6fed7e0369c1452fcaf5cd5cd479
gabrielblain <gabrielblain@gmail.com>
2024-11-12 08:53:40 -0300

    New data for reducing examples running time

---
+++ R/rainTS4.R
@@ -0,0 +1,15 @@
+#'  Rainfall Aggregated at TS equal to four
+#'
+#' Rainfall amounts aggregated using `TSaggreg`. Campinas state of Sao Paulo, Brazil.
+#'
+#' @format ## `rainTS4`
+#' A matrix with 4 columns and 2109 rows:
+#' \describe{
+#'   \item{Year}{Year}
+#'   \item{Month}{Month}
+#'   \item{quasiWeek}{Four quasi-weekly periods in each month}
+#'   \item{rain.at.TS4}{Rainfall amounts aggregated at TS equal to four}
+#'  ...
+#' }
+#' @source <https://psl.noaa.gov/>
+"rainTS4"
