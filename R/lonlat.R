254f55fcac123d75db270fe67fe7bd4e549c837c
gabrielblain <gabrielblain@gmail.com>
2025-01-17 09:57:45 -0300

    Adding new data files

---
+++ R/lonlat.R
@@ -0,0 +1,14 @@
+#' Coordinates for Entire Brazil
+#'
+#' This dataset provides coordinates for Brazil at a spatial resolution of 0.5 x 0.5 degrees.
+#'
+#' @format ## `lonlat`
+#' A matrix with 2841 rows and 2 columns:
+#' \describe{
+#'   \item{lon}{Longitude in decimal degrees}
+#'   \item{lat}{Latitude in decimal degrees}
+#'  ...
+#' }
+#' @source Data generated using information from the Instituto Brasileiro de Geografia e Estatística (IBGE).
+#' For more information, visit: <https://www.ibge.gov.br/>
+"lonlat"
